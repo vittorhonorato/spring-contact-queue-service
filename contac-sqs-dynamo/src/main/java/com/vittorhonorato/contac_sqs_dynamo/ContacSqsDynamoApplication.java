263fe026736813c1059a21468cf4349747ec8bde
Vitor Honorato <vittorhonoratoo@gmail.com>
2026-04-18 23:43:19 -0300

    Meu primeiro commit

---
+++ contac-sqs-dynamo/src/main/java/com/vittorhonorato/contac_sqs_dynamo/ContacSqsDynamoApplication.java
@@ -0,0 +1,13 @@
+package com.vittorhonorato.contac_sqs_dynamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContacSqsDynamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContacSqsDynamoApplication.class, args);
+	}
+
+}
---
+++ contac-sqs-dynamo/src/test/java/com/vittorhonorato/contac_sqs_dynamo/ContacSqsDynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittorhonorato.contac_sqs_dynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContacSqsDynamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
